f029c8ea2239bc08cb482b3dffd8ef1726af7be8
Alan <fenglan18@outlook.com>
2025-04-07 21:58:28 +0200

    remove aux in transfuser

--- navsim/agents/transfuser/transfuser_model.py
+++ navsim/agents/transfuser/transfuser_model.py
@@ -38,33 +38,33 @@ class TransfuserModel(nn.Module):
         self._bev_downscale = nn.Conv2d(512, config.tf_d_model, kernel_size=1)
         self._status_encoding = nn.Linear(4 + 2 + 2, config.tf_d_model)
 
-        self._bev_semantic_head = nn.Sequential(
-            nn.Conv2d(
-                config.bev_features_channels,
-                config.bev_features_channels,
-                kernel_size=(3, 3),
-                stride=1,
-                padding=(1, 1),
-                bias=True,
-            ),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(
-                config.bev_features_channels,
-                config.num_bev_classes,
-                kernel_size=(1, 1),
-                stride=1,
-                padding=0,
-                bias=True,
-            ),
-            nn.Upsample(
-                size=(
-                    config.lidar_resolution_height // 2,
-                    config.lidar_resolution_width,
-                ),
-                mode="bilinear",
-                align_corners=False,
-            ),
-        )
+        # self._bev_semantic_head = nn.Sequential(
+        #     nn.Conv2d(
+        #         config.bev_features_channels,
+        #         config.bev_features_channels,
+        #         kernel_size=(3, 3),
+        #         stride=1,
+        #         padding=(1, 1),
+        #         bias=True,
+        #     ),
+        #     nn.ReLU(inplace=True),
+        #     nn.Conv2d(
+        #         config.bev_features_channels,
+        #         config.num_bev_classes,
+        #         kernel_size=(1, 1),
+        #         stride=1,
+        #         padding=0,
+        #         bias=True,
+        #     ),
+        #     nn.Upsample(
+        #         size=(
+        #             config.lidar_resolution_height // 2,
+        #             config.lidar_resolution_width,
+        #         ),
+        #         mode="bilinear",
+        #         align_corners=False,
+        #     ),
+        # )
 
         tf_decoder_layer = nn.TransformerDecoderLayer(
             d_model=config.tf_d_model,
@@ -75,11 +75,11 @@ class TransfuserModel(nn.Module):
         )
 
         self._tf_decoder = nn.TransformerDecoder(tf_decoder_layer, config.tf_num_layers)
-        self._agent_head = AgentHead(
-            num_agents=config.num_bounding_boxes,
-            d_ffn=config.tf_d_ffn,
-            d_model=config.tf_d_model,
-        )
+        # self._agent_head = AgentHead(
+        #     num_agents=config.num_bounding_boxes,
+        #     d_ffn=config.tf_d_ffn,
+        #     d_model=config.tf_d_model,
+        # )
 
         self._trajectory_head = TrajectoryHead(
             num_poses=trajectory_sampling.num_poses,
@@ -111,18 +111,18 @@ class TransfuserModel(nn.Module):
         query = self._query_embedding.weight[None, ...].repeat(batch_size, 1, 1)
         query_out = self._tf_decoder(query, keyval)
 
-        bev_semantic_map = self._bev_semantic_head(bev_feature_upscale)
+        # bev_semantic_map = self._bev_semantic_head(bev_feature_upscale)
         trajectory_query, agents_query = query_out.split(self._query_splits, dim=1)
 
-        output: Dict[str, torch.Tensor] = {"bev_semantic_map": bev_semantic_map}
+        #output: Dict[str, torch.Tensor] = {"bev_semantic_map": bev_semantic_map}
+        output: Dict[str, torch.Tensor] = {}
         trajectory = self._trajectory_head(trajectory_query)
         output.update(trajectory)
 
-        agents = self._agent_head(agents_query)
-        output.update(agents)
+        # agents = self._agent_head(agents_query)
+        # output.update(agents)
 
         return output
-
 
 class AgentHead(nn.Module):
     """Bounding box prediction head."""
